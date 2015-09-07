2072e97fbf1f00af87dbc1296d833e875ce714ab
melliebe <melanie.z.bender@gmail.com>
2015-09-07 15:41:48 -0700

    Add Social Discovery

--- assets/flare.js
+++ assets/flare.js
@@ -53,7 +53,21 @@ var bubbleData = {
 
               "size" : 1000
             },
-            { "name": "Social Discovery", "size": 1000 },
+            {
+              "name": "Social Discovery",
+              "desc": {
+                "paragraphs": [
+                  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+                ],
+
+                "related" : [
+                  "Identity",
+                  "Brands",
+                  "Values"
+                ],
+              },
+              "size": 1000
+            },
             { "name": "Democratized Influence", "size": 1000 },
             { "name": "Post-Privacy World", "size": 1000 }
           ]
